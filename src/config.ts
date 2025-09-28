f4e42ea4723efcef76187cdc44528db17e6a3ac1
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2025-09-28 23:32:33 +0300

    added new rule to gitignore and change confog.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
-export const API_KEY = import.meta.env.VITE_API_KEY as string;
+export const API_KEY = import.meta.env.VITE_API_KEY;
 export const API_URL = 'https://fortniteapi.io/v2/shop';
+console.log(API_KEY);
